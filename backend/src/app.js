dba7cc77faf47e12084a87472cf3911fc7948139
PEETHANI SATYA DURGA RAO <231454932+satya24mai26-gif@users.noreply.github.com>
2026-07-28 11:21:07 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,64 +1,38 @@
 import express from 'express'
-
 import cors from 'cors'
-
 import cookieParser from 'cookie-parser'
-
 import authRoutes from "./routes/authRoutes.js";
-
 import invitationRoutes from "./routes/invitationRoutes.js";
 
 const app = express()
 
-
-
 /*
 MIDDLEWARE
 */
-
 app.use(
-
   cors({
-
-    origin: true,
-
-    credentials: true
-
+    origin: [
+      'http://localhost:5173', 
+      'https://cinematic-invitation-platform.vercel.app' // Replace with your exact Vercel URL
+    ],
+    credentials: true // This allows the cookies to pass through
   })
-
 )
 
 app.use(express.json())
-
 app.use(cookieParser())
 
-app.use(
-  '/api/auth',
-  authRoutes
-)
-
-app.use(
-    '/api/invitations',
-    invitationRoutes
-  )
+app.use('/api/auth', authRoutes)
+app.use('/api/invitations', invitationRoutes)
 
 /*
 TEST ROUTE
 */
-
 app.get('/', (req, res) => {
-
   res.json({
-
     success: true,
-
-    message:
-      'Wedding Invitation API Running'
-
+    message: 'Wedding Invitation API Running'
   })
-
 })
-
-
 
 export default app
