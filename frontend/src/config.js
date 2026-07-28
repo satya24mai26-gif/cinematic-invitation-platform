9ce9dce8c8f2c6e3196cac5224910106c3b1154b
PEETHANI SATYA DURGA RAO <231454932+satya24mai26-gif@users.noreply.github.com>
2026-07-28 11:41:27 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,5 @@
-export const API_URL =
-
-  import.meta.env.VITE_API_URL
+// If running locally (npm run dev), use your .env variable.
+// If running on Vercel, use an empty string "" to trigger the vercel.json proxy.
+export const API_URL = import.meta.env.DEV 
+  ? import.meta.env.VITE_API_URL 
+  : "";
